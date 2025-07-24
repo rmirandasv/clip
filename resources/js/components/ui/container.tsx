58f7fa4da65f4e01861a020f2b99c137e7c68b82
Ronald Miranda <ronaldmirandasv@gmail.com>
2025-07-23 22:14:34 -0600

    refactor: remove redundant class from Container component in UI

--- resources/js/components/ui/container.tsx
+++ resources/js/components/ui/container.tsx
@@ -2,6 +2,6 @@ import { cn } from "@/lib/utils";
 
 export function Container({ children, className }: { children: React.ReactNode, className?: string }) {
     return (
-        <div className={cn("flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto", className)}>{children}</div>
+        <div className={cn("flex h-full flex-1 flex-col gap-4 p-4 overflow-x-auto", className)}>{children}</div>
     );
 }
